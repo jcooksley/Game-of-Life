9bffdc811d423314c6d3065288d9f01349489ec6
jcooksley <cooksley02@gmail.com>
2021-09-29 22:18:39 +1000

    basic board display method

--- main.rb
+++ main.rb
@@ -1,13 +1,29 @@
 def setup_board
-    board = Array.new(10){Array.new(10)}
-
+    board = Array.new(10){Array.new(100)}
     board.each_with_index do |row, row_index|
         row.each_with_index do |cell, cell_index|
           board[row_index][cell_index] = rand(0..1)
         end
     end
-    
-    print board
+    return board
 end
 
+def display_board
+    new_board = setup_board()
+    new_board.each_with_index do |row, row_index|
+        row.each_with_index do |cell, cell_index|
+            if cell_index == 0
+                print "\n"
+            end
+            if new_board[row_index][cell_index] == 1
+                print "█"
+            elsif new_board[row_index][cell_index] == 0
+                print " "
+            end
+        end
+    end
+end
+
+
 setup_board()
+display_board()
